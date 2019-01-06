93a1c0786435b1173479e72f1c55878b76740ad0
Junior Albuquerque <albuquerque-junior@hotmail.com>
2019-01-06 12:15:51 -0200

    primeiro commit

---
+++ hs/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './menu/navbar.jsx';
+import './App.css';
+import Navbar from './menu/navbar.jsx';
+import logo from './home-school.png';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faIgloo } from '@fortawesome/free-solid-svg-icons';
+library.add(faIgloo)
+
+
+class App extends Component {
+  render() {
+    return (
+      <div height="100%" className="App">
+        <Navbar />
+        <div className="container">
+          <div className="col-md-4 item">
+          </div>
+          <div className="col-md-4 item">
+          </div>
+          <div className="col-md-4 item">
+          </div>
+          <div className="col-md-4 item">
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
